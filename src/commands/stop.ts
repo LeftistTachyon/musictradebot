fe7936518f08048bcb68992a763206866e992e0b
Gui Guy <jedandeganwang1999@gmail.com>
2022-12-30 00:09:24 -0600

    Forgot a file

---
+++ src/commands/stop.ts
@@ -0,0 +1,19 @@
+import { SlashCommandBuilder } from "discord.js";
+import { kill } from "..";
+import { DiscordCommand } from "../types";
+import { isBotOwner } from "../util";
+
+const stop: DiscordCommand = {
+  data: new SlashCommandBuilder()
+    .setName("stop")
+    .setDescription("Kill the bot")
+    .setDMPermission(true),
+
+  async execute(interaction) {
+    if (!isBotOwner(interaction)) return;
+
+    await kill();
+  },
+};
+
+export default stop;
