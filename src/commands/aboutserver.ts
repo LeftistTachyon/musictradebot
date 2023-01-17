956e0c7d033a78764531fb7fbde6e43225090ad7
Gui Guy <jedandeganwang1999@gmail.com>
2023-01-17 16:19:05 -0600

    Add printout of participants in /aboutserver

--- src/commands/aboutserver.ts
+++ src/commands/aboutserver.ts
@@ -17,44 +17,58 @@ const aboutserver: DiscordCommand = {
     await interaction.deferReply();
 
     const server = await getServer(new Long(interaction.guildId));
+    if (!server) {
+      await interaction.editReply(
+        "Something went horribly wrong! Please let the server owner know that you can't fetch server settings!"
+      );
+      return;
+    }
 
-    await interaction.editReply(
-      server
-        ? {
-            embeds: [
-              new EmbedBuilder()
-                .setTitle(server.name)
-                .setThumbnail(interaction.guild.iconURL())
-                .addFields(
-                  {
-                    name: "Announcements channel",
-                    value: `<#${server.announcementsChannel}>`,
-                  },
-                  {
-                    name: "Announcements mentionable role",
-                    value: server.pingableRole
-                      ? `<@&${server.pingableRole}>`
-                      : "none",
-                  }
-                )
-                .addFields(
-                  {
-                    name: "Song review period",
-                    value: Duration.fromObject({
-                      minutes: server.commentPeriod,
-                    }).toFormat("h 'hours,' m 'minutes'"),
-                  },
-                  {
-                    name: "Last-call reminder",
-                    value: Duration.fromObject({
-                      minutes: server.reminderPeriod,
-                    }).toFormat("h 'hours,' m 'minutes before ending'"),
-                  }
-                ),
-            ],
-          }
-        : "Something went horribly wrong! Please let the server owner know that you can't fetch server settings!"
-    );
+    let participantsValue = "";
+    for (let idx = 0; idx < server.users.length; idx++) {
+      const user = `<@${server.users[idx].uid}>\n`;
+      if (participantsValue.length + user.length > 1006) {
+        participantsValue += `...and ${server.users.length - idx} more`;
+        break;
+      }
+
+      participantsValue += user;
+    }
+
+    await interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle(server.name)
+          .setThumbnail(interaction.guild.iconURL())
+          .addFields(
+            {
+              name: "Announcements channel",
+              value: `<#${server.announcementsChannel}>`,
+            },
+            {
+              name: "Announcements mentionable role",
+              value: server.pingableRole
+                ? `<@&${server.pingableRole}>`
+                : "none",
+            }
+          )
+          .addFields(
+            {
+              name: "Song review period",
+              value: Duration.fromObject({
+                minutes: server.commentPeriod,
+              }).toFormat("h 'hours,' m 'minutes'"),
+            },
+            {
+              name: "Last-call reminder",
+              value: Duration.fromObject({
+                minutes: server.reminderPeriod,
+              }).toFormat("h 'hours,' m 'minutes before ending'"),
+            }
+          )
+          .addFields({ name: "Participants", value: participantsValue }),
+      ],
+    });
   },
 };
 
