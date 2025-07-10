94b0a147cb4190500fb5c4a3a9a4f046b1350f3c
Gui Guy <jedandeganwang1999@gmail.com>
2025-07-09 23:27:35 -0500

    Fix edge cases of graph generation to avoid self-loops

--- src/util.ts
+++ src/util.ts
@@ -76,7 +76,7 @@ export function createTrade(server: Server, duration: number): Trade {
   const fromUnchosen = users.slice(),
     toUnchosen = users.slice(),
     trades: { from: Long; to: Long }[] = [];
-  while (fromUnchosen.length > 1) {
+  while (fromUnchosen.length > 2) {
     let fromIdx, toIdx;
     do {
       fromIdx = randomInt(fromUnchosen.length);
@@ -88,7 +88,36 @@ export function createTrade(server: Server, duration: number): Trade {
       to: toUnchosen.splice(toIdx, 1)[0],
     });
   }
-  trades.push({ from: fromUnchosen[0], to: toUnchosen[0] });
+  // ensure no lone wolf at end
+  const included = toUnchosen.filter((a) => fromUnchosen.includes(a)),
+    excluded = toUnchosen.filter((a) => !fromUnchosen.includes(a));
+  switch (included.length) {
+    case 2:
+      // A <=> B
+      trades.push({ from: included[0], to: included[1] });
+      trades.push({ from: included[1], to: included[0] });
+      break;
+    case 1:
+      // A => B => C
+      if (fromUnchosen[0] === included[0]) {
+        trades.push({ from: fromUnchosen[0], to: excluded[0] });
+        trades.push({ from: fromUnchosen[1], to: included[0] });
+      } else {
+        trades.push({ from: fromUnchosen[1], to: excluded[0] });
+        trades.push({ from: fromUnchosen[0], to: included[0] });
+      }
+      break;
+    case 0:
+      // A => B, C => D
+      trades.push({
+        from: fromUnchosen.splice(randomInt(fromUnchosen.length), 1)[0],
+        to: toUnchosen.splice(randomInt(toUnchosen.length), 1)[0],
+      });
+      trades.push({ from: fromUnchosen[0], to: toUnchosen[0] });
+      break;
+    default:
+      throw new Error("invalid trade state");
+  }
 
   // calculate start and end times
   const start = DateTime.now().startOf("day"),
